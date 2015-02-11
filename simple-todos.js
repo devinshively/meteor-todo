8167c5a082bc45cf25b638c786facf64e7e83af9
Devin Shively <devinshively@devins-mac-pro.attlocal.net>
2015-02-11 09:17:39 -0800

    Finished tutorial

--- simple-todos.js
+++ simple-todos.js
@@ -1,6 +1,7 @@
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
+    Meteor.subscribe("tasks");
 
     Template.body.helpers({
         tasks: function() {
@@ -33,12 +34,21 @@ if (Meteor.isClient) {
         }
     });
 
+    Template.task.helpers({
+        isOwner: function() {
+            return this.owner === Meteor.userId();
+        }
+    });
+
     Template.task.events({
         "click .toggle-checked": function() {
             Meteor.call("setChecked", this._id, !this.checked);
         },
         "click .delete": function() {
             Meteor.call("deleteTask", this._id);
+        },
+        "click .toggle-private": function() {
+            Meteor.call("setPrivate", this._id, !this.private);
         }
     });
 
@@ -61,15 +71,33 @@ Meteor.methods({
         });
     },
     deleteTask: function(taskId) {
+        var task = Tasks.findOne(taskId);
+        if(task.private && task.owner !== Meteor.userId()) {
+            throw new Meteor.Error("not-authorized");
+        }
         Tasks.remove(taskId);
     },
     setChecked: function(taskId, setChecked) {
+        var task = Tasks.findOne(taskId);
+        if(task.private && task.owner !== Meteor.userId()) {
+            throw new Meteor.Error("not-authorized");
+        }
         Tasks.update(taskId, {$set: {checked: setChecked}});
+    },
+    setPrivate: function(taskId, setToPrivate) {
+        var task = Tasks.findOne(taskId);
+        if(task.owner !== Meteor.userId()) {
+            throw new Meteor.Error("not-authorized");
+        }
+        Tasks.update(taskId, {$set: {private: setToPrivate}});
     }
 });
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+    Meteor.startup(function () {
     // code to run on server at startup
-  });
+    });
+    Meteor.publish("tasks", function() {
+        return Tasks.find({$or: [{private: {$ne: true}},{owner: this.userId}]});
+    });
 }
